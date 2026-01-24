159818731308bf3d0489eb9257633ffedf50ace1
Pratham Pawar <vu4f2324148@pvppcoe.ac.in>
2026-01-24 22:06:50 +0530

    Fix index.html and clean service worker registration

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,32 @@
 const CACHE_NAME = "simpleshop-cache-v1";
 
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/cart.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./cart.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
 ];
 
-// Install event
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// Activate event
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
-// Fetch event
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
